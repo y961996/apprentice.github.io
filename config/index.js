4edfe264758ffc057d74b94b7a641f77c9eee584
Deneme <yunus10atahan@gmail.com>
2018-05-08 10:43:33 +0300

    .

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/y961996.github.io',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
